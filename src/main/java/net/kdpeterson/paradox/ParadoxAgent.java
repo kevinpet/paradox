42104cb32e8ac2c58228c3b5b450408bd6929561
Kevin Peterson <kevin@kdpeterson.net>
2016-03-19 14:14:42 -0700

    Add offset

--- src/main/java/net/kdpeterson/paradox/ParadoxAgent.java
+++ src/main/java/net/kdpeterson/paradox/ParadoxAgent.java
@@ -2,9 +2,13 @@ package net.kdpeterson.paradox;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeUtils;
+import org.joda.time.convert.DurationConverter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ParadoxAgent {
 
@@ -18,6 +22,14 @@ public class ParadoxAgent {
         DateTime supplied = ISODateTimeFormat.dateTimeParser().withOffsetParsed().parseDateTime(dateString);
         log.info("Setting fixed time of {} (supplied: {})", dateString, supplied);
         DateTimeUtils.setCurrentMillisFixed(supplied.getMillis());
+        break;
+      case "offset":
+        int seconds = Integer.parseInt(fields[1]);
+        log.info("Setting offset of {} seconds", seconds);
+        DateTimeUtils.setCurrentMillisOffset(seconds * 1000);
+        break;
+      default:
+        log.warn("Unknown time setting: " + agentArgs);
         break;
     }
   }
--- src/test/java/net/kdpeterson/paradox/ParadoxAgentTest.java
+++ src/test/java/net/kdpeterson/paradox/ParadoxAgentTest.java
@@ -2,21 +2,24 @@ package net.kdpeterson.paradox;
 
 import static org.junit.Assert.*;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
 import org.joda.time.DateTimeZone;
+import org.joda.time.Seconds;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 
 public class ParadoxAgentTest {
 
-  @Test
-  public void testStringParsing() {
-    String dateString = "2016-01-01T12:00:00.000Z";
-    DateTime parsed = ISODateTimeFormat.dateTimeParser().withOffsetParsed().parseDateTime(dateString);
-    DateTime expected = new DateTime(2016, 1, 1, 12, 0, 0, 0, DateTimeZone.UTC);
-    assertEquals(expected, parsed);
+  Logger log = LoggerFactory.getLogger(ParadoxAgentTest.class);
+
+  @Before @After
+  public void beforeAndAfter() {
+    DateTimeUtils.setCurrentMillisSystem();
   }
 
   @Test
@@ -28,9 +31,11 @@ public class ParadoxAgentTest {
 
   @Test
   public void testOffset() {
-    ParadoxAgent.premain("fixed=2016-01-01T12:00:00.000Z");
-    DateTime now = new DateTime(DateTimeZone.UTC);
-    assertEquals(new DateTime(2016, 1, 1, 12, 0, 0, 0, DateTimeZone.UTC), now);
+    DateTime realNow = new DateTime(System.currentTimeMillis());
+    ParadoxAgent.premain("offset=-100");
+    DateTime now = new DateTime();
+    int offset = Seconds.secondsBetween(realNow, now).getSeconds();
+    assertTrue("expected " + -100 + ", was " + offset, -101 <= offset && offset <= -99);
   }
 
 }
