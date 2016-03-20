295dcf692a70a28b8e90fe975ebcf949ab18eb79
Kevin Peterson <kevinp@wealthfront.com>
2016-03-20 13:54:37 -0700

    Support for random dates over a day or year

--- src/main/java/net/kdpeterson/paradox/ParadoxAgent.java
+++ src/main/java/net/kdpeterson/paradox/ParadoxAgent.java
@@ -2,11 +2,15 @@ package net.kdpeterson.paradox;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeUtils;
+import org.joda.time.DateTimeUtils.MillisProvider;
+import org.joda.time.Interval;
+import org.joda.time.Period;
 import org.joda.time.convert.DurationConverter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,9 +32,53 @@ public class ParadoxAgent {
         log.info("Setting offset of {} seconds", seconds);
         DateTimeUtils.setCurrentMillisOffset(seconds * 1000);
         break;
+      case "random":
+        DateTimeUtils.setCurrentMillisProvider(random(fields[1]));
+        break;
       default:
         log.warn("Unknown time setting: " + agentArgs);
         break;
+    }
+  }
+
+  static RandomMillisProvider random(String range) {
+    String seed = System.getProperty("paradox.random.seed");
+    Random random = seed != null ? new Random(seed.hashCode()) : new Random();
+    String anchor = System.getProperty("paradox.random.anchor");
+    DateTime now = anchor != null ? new DateTime(anchor) : new DateTime();
+    {
+      Interval interval;
+      switch (range) {
+        case "year":
+          DateTime start = new DateTime(now.getYear(), 1, 1, 0, 0, 0, 0, now.getZone());
+          DateTime end = new DateTime(now.getYear() + 1, 1, 1, 0, 0, 0, 0, now.getZone());
+          interval = new Interval(start, end);
+          log.info("Providing random dates over {}.", interval);
+          return new RandomMillisProvider(random, interval);
+        case "day":
+        default:
+          interval = new Interval(now.toLocalDate().toDateTimeAtStartOfDay(),
+                  now.toLocalDate().plusDays(1).toDateTimeAtStartOfDay());
+          log.info("Providing random dates over {}.", interval);
+          return new RandomMillisProvider(random, interval);
+      }
+    }
+  }
+
+  static class RandomMillisProvider implements MillisProvider {
+    private final Random random;
+    private final Interval interval;
+    RandomMillisProvider(Random random, Interval interval) {
+      this.random = random;
+      this.interval = interval;
+    }
+
+    @Override
+    public long getMillis() {
+      double r = random.nextDouble();
+      long im = interval.getEndMillis() - interval.getStartMillis();
+      long offset = (long) (r * (double) im);
+      return interval.getStartMillis() + offset;
     }
   }
 }
--- src/test/java/net/kdpeterson/paradox/ParadoxAgentTest.java
+++ src/test/java/net/kdpeterson/paradox/ParadoxAgentTest.java
@@ -1,10 +1,8 @@
 package net.kdpeterson.paradox;
 
 import static org.junit.Assert.*;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeUtils;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Seconds;
+
+import org.joda.time.*;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.*;
 import org.slf4j.Logger;
@@ -12,6 +10,13 @@ import org.slf4j.LoggerFactory;
 
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ParadoxAgentTest {
 
@@ -38,4 +43,39 @@ public class ParadoxAgentTest {
     assertTrue("expected " + -100 + ", was " + offset, -101 <= offset && offset <= -99);
   }
 
+  private static class MinMaxConsumer<T extends Comparable<? super T>> implements Consumer<T> {
+    T min;
+    T max;
+
+    @Override
+    public void accept(T t) {
+      if (min == null || t.compareTo(min) < 0) {
+        min = t;
+      }
+      if (max == null || t.compareTo(max) > 0) {
+        max = t;
+      }
+    }
+  }
+  @Test
+  public void testRandomDay() {
+    DateTime realNow = new DateTime(System.currentTimeMillis());
+    ParadoxAgent.premain("random=day");
+    MinMaxConsumer<DateTime> minMax = new MinMaxConsumer<>();
+    IntStream.range(1, 20).mapToObj(i -> new DateTime()).forEach(minMax);
+    Duration range = new Duration(minMax.min, minMax.max);
+    assertTrue("Expected min...max to span 12-24 hours, but was " + range,
+            Duration.standardHours(12).isShorterThan(range) && Duration.standardHours(24).isLongerThan(range));
+  }
+
+  @Test
+  public void testRandomYear() {
+    DateTime realNow = new DateTime(System.currentTimeMillis());
+    ParadoxAgent.premain("random=year");
+    MinMaxConsumer<DateTime> minMax = new MinMaxConsumer<>();
+    IntStream.range(1, 20).mapToObj(i -> new DateTime()).forEach(minMax);
+    Duration range = new Duration(minMax.min, minMax.max);
+    assertTrue("Expected min...max to span 180-366 days, but was " + range,
+            Duration.standardDays(180).isShorterThan(range) && Duration.standardDays(366).isLongerThan(range));
+  }
 }
