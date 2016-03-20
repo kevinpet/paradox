ce2d1d92d7ce36c3d5d9f3342a2c2b2debd47b19
Kevin Peterson <kevinp@wealthfront.com>
2016-03-20 13:55:11 -0700

    Remove unused parameter from acceptance test

--- acceptance.sh
+++ acceptance.sh
@@ -2,7 +2,7 @@
 
 jar=target/paradox-1.0-SNAPSHOT.jar
 expected=2016-01-01T12:00:00.000Z
-command="java -javaagent:$jar=fixed=$expected -jar $jar -Dparadox.mode=fixed"
+command="java -javaagent:$jar=fixed=$expected -jar $jar"
 actual=`$command 2>/dev/null | tail -1`
 if [ $actual == $expected ]; then
 echo Set fixed value: passed.
